5fa66f23c4c7477788a296306fb285263ff10b4a
carlos perez <cpere123@cnm.edu>
2021-12-28 10:01:44 -0700

    added script.js content

--- script.js
+++ script.js
@@ -10,11 +10,30 @@ function initShoppingList (){
 }
 
 function handleItemForm (event, formRef) {
-    if (event.preventDefault() ) {
+    if (event.preventDefault()) {
         event.preventDefault();
     }
 
-    console.log("Event: ", event);
-
+    addItemToShoppingList()
     return false;
 }
+
+
+function addItemToShoppingList () {
+    let itemName = document.getElementById("item-name");
+    let itemAmount = document.getElementById("item-amount");
+
+
+    let itemHtml = createListItemHtml();
+    console.log("Item Html: ", itemHtml);
+    let itemListRef= document.getElementById("shopping-list")
+    itemListRef.insertAdjacentHTML("afterend ", itemHtml)
+
+
+
+}
+
+function createListItemHtml () {
+    return '<li> ItemNameAmount </li>'
+};
+
