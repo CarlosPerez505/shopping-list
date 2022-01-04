d754b8a661b419cfd3f0b88ff432786b4bcca140
carlos perez <cpere123@cnm.edu>
2022-01-04 12:32:20 -0700

    added to js file

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function handleItemForm (event, formRef) {
 }
 
 function createItemListHtml () {
-    return '<li> ${itemName} - ${itemAmount} <button>type="button">Delete Item</button> </li>';
+    return '<li> ${itemName} - ${itemAmount} <button type = "button" > Delete Item </button> </li>';
 }
 
 
