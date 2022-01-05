106ff31e9af61664f864efed69ec08e0241d0f42
carlos perez <cpere123@cnm.edu>
2022-01-05 12:00:13 -0700

    subracted from js file

--- script.js
+++ script.js
@@ -11,31 +11,12 @@ function initShoppingList () {
 }
 
 function handleItemForm (event, formRef) {
-    if (event.preventDefault) {
+    if (event.preventDefault()) {
         event.preventDefault();
     }
     addItemToShoppingList();
-
+    console.log("event: ", event)
     return false;
-}
-
- function addItemToShoppingList () {
-    let itemName = document.getElementById("item-name");
-    let itemAmount = document.getElementById("item-amount")
-
-    // creates list item html and appends to page
-    let itemHtml = createItemListHtml(itemName, itemAmount);
-     console.log("Item Html: ", itemHtml)
-     let itemListRef = document.getElementById("shopping-list");
-     itemListRef.insertAdjacentHTML("afterend", itemHtml)
-
-}
-
-
-function createItemListHtml (itemName, itemAmount) {
-    return `<li>
-                ${itemName.value} - ${itemAmount.value}
-            </li>`;
 }
 
 
