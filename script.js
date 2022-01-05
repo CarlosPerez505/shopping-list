212b80fc69159f1ff98b6aedf9ff02c1ad96c78a
carlos perez <cpere123@cnm.edu>
2022-01-05 12:11:25 -0700

    subracted from js file

--- script.js
+++ script.js
@@ -4,21 +4,7 @@ window.onload= function () {
 
 function initShoppingList () {
     let form = document.getElementById("item-form");
-
-    form.addEventListener("submit", (event) => {
-        handleItemForm ();
-    });
-}
-
-function handleItemForm (event, formRef) {
-    if (event.preventDefault()) {
-        event.preventDefault();
-    }
-    addItemToShoppingList();
-    console.log("hello world")
-    return false;
-}
-
+    console.log(form)
 
 
 
