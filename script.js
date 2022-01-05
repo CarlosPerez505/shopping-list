d3eeb8c77111274db224393b44b7ce7728fdca72
carlos perez <cpere123@cnm.edu>
2022-01-05 09:37:33 -0700

    added to js file

--- script.js
+++ script.js
@@ -21,19 +21,41 @@ function handleItemForm (event, formRef) {
  function addItemToShoppingList () {
     let itemName = document.getElementById("item-name");
     let itemAmount = document.getElementById("item-amount")
+    let id = getRandomInt(0, 10000000)
 
-    let itemHtml = createItemListHtml(itemName.value, itemAmount.value);
+
+    // creates list item html and appends to page
+    let itemHtml = createItemListHtml(itemName.value, itemAmount.value, id);
      console.log("Item Html: ", itemHtml)
      let itemListRef = document.getElementById("shopping-list");
      itemListRef.insertAdjacentHTML("afterend", itemHtml)
 
+     setDeleteButtonEvent(id)
+
 }
 
+
+function setDeleteButtonEvent(id) {
+    let deleteButton = document.getElementById("button"+id);
+    deleteButton.addEventListener("click", ()=> {
+        console.log("Delete Button Works");
+    })
+
+}
+
+
+
 function createItemListHtml (itemName, itemAmount) {
-    return `<li>
+    return `<li id="item${id}">
                 ${itemName} - ${itemAmount}
-                <button type="button"> Delete Item </button>
+                <button id="button${id}" type="button"> Delete Item </button>
             </li>`;
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 
